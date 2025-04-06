ded68e3d5eaae910f1fb864de32540e69aa03979
Max <186575394+mxbv@users.noreply.github.com>
2025-04-06 18:49:17 +0300

    3.1.2

--- docs/_app/immutable/entry/start.BCdwdlyB.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/Q7tdRgZP.js";export{o as load_css,r as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,27 @@
-import adapter from "@sveltejs/adapter-static";
+// import adapter from "@sveltejs/adapter-static";
 
-const config = {
+// const config = {
+//   kit: {
+//     adapter: adapter({
+//       fallback: "index.html", // fallback для SPA
+//     }),
+//   },
+// };
+
+// export default config;
+
+import adapter from '@sveltejs/adapter-static';
+
+export default {
   kit: {
     adapter: adapter({
-      fallback: "index.html", // fallback для SPA
+      fallback: 'index.html' // важная строка для GitHub Pages
     }),
-  },
+    paths: {
+      base: '' // так как ты хочешь использовать root
+    },
+    prerender: {
+      entries: ['*']
+    }
+  }
 };
-
-export default config;
